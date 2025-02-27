df79d1ec09fbac195fc20ad9b171df3701df3740
Abdelouahab Bella <abdobella977@gmail.com>
2025-02-27 17:30:49 +0000

    Update Vite configuration and GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite_react_github_secrets_manager_5rp2vv/',
+  base: process.env.NODE_ENV === 'production' ? '/vite_react_github_secrets_manager_5rp2vv/' : '/',
 })
